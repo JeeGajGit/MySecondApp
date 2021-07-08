59903aaca549d2fa2597e3a098d967bbe02af116
JeeGajGit <87121965+JeeGajGit@users.noreply.github.com>
2021-07-08 15:43:31 +0530

    Update MainApplication.java (#2)

--- MainApplication.java
+++ MainApplication.java
@@ -1,8 +1,10 @@
-Public class MainApplication{
+public class MainApplication{
 
 public static void main(String args[]){
 
 System.out.println(String args[]);
+  
+  System.out.println("checking merging changes");
 
 }
 
